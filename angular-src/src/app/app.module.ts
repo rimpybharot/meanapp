da05eac74741b9c5f1b8d95ac1d1d5722a6e905b
Rimpy Bharot <rbverse21@gmail.com>
2017-06-05 02:21:42 -0500

    Adding Form for Registration

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileComponent } from './components/profile/profile.component';
 
+import {ValidateService} from "./services/validate.service";
+
 const appRoutes: Routes = [
   {path: '' , component: HomeComponent},
   {path: 'register', component: RegisterComponent},
@@ -36,7 +38,7 @@ const appRoutes: Routes = [
     HttpModule,
     RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ValidateService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
